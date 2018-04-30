55278edfb2da5bdfa585e7c5e1299df5c9f23a0f
javi <javoraptor@gmail.com>
2018-04-29 23:44:22 -0500

    Refactor path to request param in controller/utils

--- src/main/java/com/nasa/controllers/ImageController.java
+++ src/main/java/com/nasa/controllers/ImageController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nasa.service.ImageService;
@@ -15,17 +16,17 @@ import com.nasa.service.ImageService;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
-@CrossOrigin
+@CrossOrigin //allows react server to connect
 @Slf4j
 public class ImageController {
 	
 	@Autowired
 	ImageService imageService;
 
-	@GetMapping("/fetch-images/date/{date}/camera-list/{cameraList}")
+	@GetMapping("/fetch-images/date/{date}/camera-list")
 	public boolean fetchImages(@PathVariable("date") String date,
-								@PathVariable("cameraList") ArrayList<String> cameraList) {
-		log.info("Begining REST call with parameters: date -> "+date+" : cameraList -> " +cameraList );
+								@RequestParam("cameras") ArrayList<String> cameraList) {
+		log.info("Begining REST call with parameters: date -> "+date+" : cameraList -> " + cameraList );
 		try {
 			imageService.executeMultipleRestCalls(cameraList, date);
 		} catch (IOException e) {
--- src/main/java/com/nasa/utils/Utils.java
+++ src/main/java/com/nasa/utils/Utils.java
@@ -3,10 +3,6 @@ package com.nasa.utils;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.net.URI;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -14,43 +10,70 @@ import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
 
-import org.springframework.http.HttpRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
-
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@Component
 public class Utils {
+
+	private static String imageDirectoryName;
+
+	@Value("${image.directory.name}")
+	public void setImageDirectoryName(String dirName) {
+		imageDirectoryName = dirName;
+	}
 
 	public static void downloadImageToFile(String url, String idSource, String date) throws IOException {
 		log.info("Downloading image to file with url -> " + url + " : idSource -> " + idSource);
-		
+
 		RestTemplate restTemplate = new RestTemplate();
 		byte[] imageBytes = restTemplate.getForObject(url, byte[].class);
 		writeBytesToFile(imageBytes, idSource + ".jpg", date);
 	}
 
+	public static List<String> readFileIntoDateArray(String fileName) {
+		log.info("Reading file into date array, file name -> " + fileName);
+
+		List<String> dateList = new ArrayList<>();
+
+		try (Scanner scan = new Scanner(new File(fileName))) {
+			while (scan.hasNext()) {
+				dateList.add(convertToYearMonthDay(scan.next()));
+			}
+		} catch (Exception e) {
+			log.error("Error reading file -> " + fileName, e);
+		}
+		return dateList;
+	}
+
+	public static String convertToYearMonthDay(String temp) {
+		log.info("Converting date -> " + temp + "to correct format");
+
+		SimpleDateFormat before = new SimpleDateFormat("dd MMMM, yyyy");
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+		String dateInString = temp;
+		Date date = null;
+
+		try {
+			date = before.parse(dateInString);
+		} catch (ParseException e) {
+			log.error("Error converting date -> " + temp, e);
+		}
+		return formatter.format(date);
+	}
+
 	private static void writeBytesToFile(byte[] bFile, String fileDest, String date) throws IOException {
 		log.info("Writing image bytes to file, file destination -> " + fileDest);
 		FileOutputStream fileOuputStream = null;
-		String imageDirectoryName = "./images/";
-		String fileLocation = "./images/" + convertToYearMonthDay(date) + "/";
-		
-		File imageDirectory = new File(imageDirectoryName);
-		if (!imageDirectory.exists() && !imageDirectory.isDirectory()) {
-			if(!new File(imageDirectoryName).mkdir())
-			throw new IOException("Error creating directory with name -> " + imageDirectoryName);
-		}
-		
-		File fileDirectory = new File(fileLocation);
-		if (!fileDirectory.exists() && !fileDirectory.isDirectory()) {
-			if(!new File(fileLocation).mkdir())
-			throw new IOException("Error creating directory with date -> " + convertToYearMonthDay(date));
-		}
-		
+
+		String fileLocation = imageDirectoryName + convertToYearMonthDay(date) + "/";
+
+		createDirectories(fileLocation);
+
 		try {
 			fileOuputStream = new FileOutputStream(fileLocation + fileDest);
 			fileOuputStream.write(bFile);
@@ -69,36 +92,16 @@ public class Utils {
 		}
 
 	}
+	
+	private static void createDirectories(String fileLocation) throws IOException {
+		if (!new File(imageDirectoryName).exists())
+			if (!new File(imageDirectoryName).mkdir())
+				throw new IOException("Error creating main directory with name -> " + imageDirectoryName);
 
-	public static List<String> readFileIntoDateArray(String fileName) {
-		log.info("Reading file into date array, file name -> " + fileName);
-		
-		List<String> dateList = new ArrayList<>();
-
-		try (Scanner scan = new Scanner(new File(fileName))) {
-			while (scan.hasNext()) {
-				dateList.add(convertToYearMonthDay(scan.next()));
-			}
-		} catch (Exception e) {
-			log.error("Error reading file -> " + fileName, e);
+		File fileDirectory = new File(fileLocation);
+		if (!fileDirectory.exists() && !fileDirectory.isDirectory()) {
+			if (!new File(fileLocation).mkdir())
+				throw new IOException("Error creating directory with name -> " + fileLocation);
 		}
-		return dateList;
 	}
-
-	public static String convertToYearMonthDay(String temp) {
-		log.info("Converting date -> " + temp + "to correct format");
-		
-		SimpleDateFormat before = new SimpleDateFormat("dd MMMM, yyyy");
-		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-		String dateInString = temp;
-		Date date = null;
-
-		try {
-			date = before.parse(dateInString);
-		} catch (ParseException e) {
-			log.error("Error converting date -> " + temp , e);
-		}
-		return formatter.format(date);
-	}
-
 }
